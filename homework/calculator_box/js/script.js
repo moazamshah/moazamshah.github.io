06b56b6047f4715c52ed61ca9d9c2fc8dcc86dc9
Moazam Shah <moazamshah@moazams-mbp.lan>
2018-04-16 23:21:17 -0400

    Calculator Box Homework
    Setting up functions with math

---
+++ homework/calculator_box/js/script.js
@@ -0,0 +1,47 @@
+document.querySelector("#a10.onclick") = addTen;
+
+function addTen() {
+	tenThumb = document.querySelector("#a10").value;
+	tenCenter = 0 + tenThumb;
+}
+
+document.querySelector("#a20.onclick") = addTen;
+
+function addTen() {
+	tenThumb = document.querySelector("#a20").value;
+	tenCenter = 0 + tenThumb;
+}
+
+document.querySelector("#a30.onclick") = addTen;
+
+function addTen() {
+	tenThumb = document.querySelector("#a30").value;
+	tenCenter = 0 + tenThumb;
+
+document.querySelector("tenCenter").innerHTML = #out
+}
+
+-----
+
+document.querySelector("n10.onclick") = addTen;
+
+function moveTen() {
+	tenThumb = document.querySelector("#n10").value;
+	tenCenter = 0 + tenThumb;
+}
+
+document.querySelector("#n20.onclick") = addTen;
+
+function addTen() {
+	tenThumb = document.querySelector("#n20").value;
+	tenCenter = 0 + tenThumb;
+}
+
+document.querySelector("#n30.onclick") = addTen;
+
+function addTen() {
+	tenThumb = document.querySelector("#n30").value;
+	tenCenter = 0 + tenThumb;
+
+document.querySelector("tenCenter").innerHTML = #out
+}
