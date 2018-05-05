98c75fd013dd3d9d7edb69a204fc384562942a68
Moazam Shah <moazamshah@moazams-mbp.lan>
2018-05-05 14:33:53 -0400

    JS Pseudocode
    Final Project Setup for feature sections

---
+++ homework/Startup_Page/startup_pseudo.js
@@ -0,0 +1,41 @@
+// Pseudocode for Final Project Startup Page
+
+// Heading Space
+// As a user, I want to understand the Headline clearly 
+// so that when I load the page, each word fades up one by one.
+
+// 1.] Page loads
+// 2.] "Create your best work with us" fades in from the bottom one word at a time.
+// 3.] "Create your best work with us" remains static for the rest of the page visit.
+// 4.] If the page is loaded again, the headline is introduced through animation again.
+
+// Color changing tool
+// As a user, I want to change the color of the player, 
+// so that when I click each swatch in the colors panel, the layout adjusts respectively.
+
+// 1.] I click the orange swatch in the colors panel
+// 2.] The orange swatch receives a blue rounded border around it to signify it is active.
+// 3.] The player icon and avatar fade into orange from the starting blue color, at the exact same time.
+// 4.] If I click another color swatch, the current color fades into the newly selected color, and the blue stroke is applied to the new swatch.
+// 5.] The newly applied color remains active for the remainder of the page visit, unless another action is taken on the swatch panel.
+
+// Quality highlight Toggle
+// As a user, I want to compare standard definition imagery to HD quality 
+// so that when I drag the slider across the player shape, the image mask appears and hides accordingly.
+
+// 1.] I click and drag the red slider to the right.
+// 2.] The slider drags to the right, and with it, the standard definition image on the left expands horizontally to fill the horizontal space the slider has traveled.
+// 3.] If I click and drag the slider to the left now, the High definition image on the right expands horizontally towards the right to fill the space the slider has traveled.
+// 4.] If I click and drag the slider all the way to the left, the standard definition image is completely hidden, and the high definition image appears fully.
+// 5.] If I continue to scroll down the page, the slider remains at its most recent position.
+
+// Comment Review Space
+// As a user, I want to check off applied comments, 
+// so that when I get through all submitted comments, the video plays again.
+
+// 1.] I click add on the third comment and it slides to the bottom of the comment thread in the top right.
+// 2.] The thread animates to check off the first two comment check marks one by one.
+// 3.] The third comment check mark is highlighted in blue, but does not animate its check off.
+// 4.] If I click the third comment check mark, it animates to check off and the comments dismiss themselves to the right one by one, leaving just the player shape.
+// 5.] If I do not click the third comment check mark, it remains highlighted and stays in that state for the rest of the page visit.
+
