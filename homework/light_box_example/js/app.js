6d6a31d3e8aec5796de23f8f66a34893e7a70996
Moazam Shah <moazamshah@Moazams-MacBook-Pro.local>
2018-05-07 18:29:35 -0400

    Lightbox Example Homework

---
+++ homework/light_box_example/js/app.js
@@ -0,0 +1,17 @@
+// As a User
+// When I click an image
+// I expect a lightbox to appear in a modal
+
+<script>
+    lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true
+    })
+</script>
+
+
+// Bonus:
+
+// As a User
+// When I click on any image in the bottom row
+// I expect to see the entire row in the lightbox as a carousel
